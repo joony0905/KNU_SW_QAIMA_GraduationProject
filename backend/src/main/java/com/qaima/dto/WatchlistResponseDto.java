75e24cab22fcac3c231e25cdae118de35d8e7100
falto4829 <dmsrb7161@naver.com>
2025-11-17 16:43:05 +0900

    Update WatchlistResponseDto.java

--- backend/src/main/java/com/qaima/dto/WatchlistResponseDto.java
+++ backend/src/main/java/com/qaima/dto/WatchlistResponseDto.java
@@ -1,19 +1,32 @@
 package com.qaima.dto;
 
+import com.qaima.domain.Stock;
 import com.qaima.domain.WatchlistItem;
 import lombok.Getter;
 
 @Getter
+
 public class WatchlistResponseDto {
+
     private Long watchlistItemId;
     private Long stockId;
     private String stockName;
     private String note;
+    private String industryName;
 
     public WatchlistResponseDto(WatchlistItem item) {
+
         this.watchlistItemId = item.getWatchlistItemId();
-        this.stockId = item.getStock().getStockId();
-        this.stockName = item.getStock().getCompanyName();
         this.note = item.getNote();
+
+        Stock stock = item.getStock();
+        if (stock != null) {
+            this.stockId = stock.getStockId();
+            this.stockName = stock.getCompanyName();
+
+            if (stock.getIndustry() != null) {
+                this.industryName = stock.getIndustry().getName();
+            }
+        }
     }
 }
