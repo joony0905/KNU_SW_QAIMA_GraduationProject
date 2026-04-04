cdf156786c344328046bd71e088e38d320402f4a
joony0905 <stb1436@naver.com>
2026-04-05 07:06:28 +0900

    feat: 휴장일 판단 로직 추가

---
+++ backend/src/main/java/com/qaima/service/tradingcalendar/TradingCalendarService.java
@@ -0,0 +1,13 @@
+package com.qaima.service.tradingcalendar;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
+public interface TradingCalendarService {
+
+    boolean isTradingDay(LocalDate date, String market);
+
+    LocalDate previousTradingDay(LocalDate date, String market);
+
+    LocalDate latestTradingDay(ZonedDateTime asOf, String market);
+}
